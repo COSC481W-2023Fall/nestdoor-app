66f03c341931ff8ef9d14302d3c3ecc15dad8bf4
JWT2 <144062003+JWT2@users.noreply.github.com>
2023-10-08 13:58:52 -0400

    Password Hashers
    Ensured password hashers were properly listed in settings.py.

--- src/nestdoor/settings.py
+++ src/nestdoor/settings.py
@@ -97,6 +97,18 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+# Adding password hashing algorithms
+# Adding Argon2 as most secure password hasher
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/

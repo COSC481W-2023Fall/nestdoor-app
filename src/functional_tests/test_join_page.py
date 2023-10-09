882a5fda2eb52bfd829d9685af8879359f1b0fd0
dianlu <dhe@emich.edu>
2023-10-08 21:50:26 -0400

    React + Django + Unit & Functional Tests

--- src/functional_tests/test_join_page.py
+++ src/functional_tests/test_join_page.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 import time
 
-class TestProjectListPage (StaticLiveServerTestCase) :
+class TestJoinPage (StaticLiveServerTestCase) :
 
     def setUp(self):
         # self.browser = webdriver.Chrome('functional_tests/chromedriver')
@@ -13,7 +13,18 @@ class TestProjectListPage (StaticLiveServerTestCase) :
     def tearDown(self):
         self.browser.close()
 
-    def test_nothing(self):
-        self.browser.get(self.live_server_url)
-        time.sleep(60)
+    # def test_nothing(self):
+    #     self.browser.get(self.live_server_url)
+    #     time.sleep(60)
+
+    def test_user_is_redirected_to_view2(self):
+        self.browser.get(self.live_server_url + '/views/view1')
+        # print(self.live_server_url)
+        detail_url = self.live_server_url + reverse('name_list')
+        #print(detail_url)
+        self.browser.find_element("link text", "Go back to User List").click()
+        self.assertEquals(
+            self.browser.current_url,
+            detail_url
+        )
 

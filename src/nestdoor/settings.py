1d99ece66cf9ffeb066ebe1ae78ff7945841ae90
dianlu <dhe@emich.edu>
2023-10-19 17:01:01 -0400

    Add a backgound to homepage, a logo hover test. Modified logo hover effect.

--- src/nestdoor/settings.py
+++ src/nestdoor/settings.py
@@ -30,10 +30,11 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Edited for Digital Ocean Deployment
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 # Edited for Digital Ocean Deployment
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "nestdoor-app-zcquw.ondigitalocean.app", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Added for Digital Ocean Deployment
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
@@ -95,6 +96,7 @@ WSGI_APPLICATION = "nestdoor.wsgi.application"
 } """
 # Edited for Digital Ocean Deployment
 # ATTN: Jamie and Hailey
+DEVELOPMENT_MODE = True
 if DEVELOPMENT_MODE is True:
     DATABASES = {
         "default": {

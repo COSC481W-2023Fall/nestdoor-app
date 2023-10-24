9aeb0b8614dc9365ae7be4835e22b8142d05c50d
Madeline <asmit438@emich.edu>
2023-10-23 23:54:15 -0400

    fix crashes in loading model file

--- src/nestdoorapp/models.py
+++ src/nestdoorapp/models.py
@@ -1,4 +1,3 @@
-from tkinter import NO
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -30,48 +29,38 @@ class Post(models.Model):
     # main
     post_id = models.AutoField(primary_key=True)
     datetime_posted = models.DateTimeField()
-    posted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    posted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="posts")
     content = models.TextField()
     number_replies = models.IntegerField()
     datetime_last_edited = models.DateTimeField(null=True)
     
     # moderation
     datetime_last_moderated = models.DateTimeField(null=True)
-    last_moderated_by = models(User, null=True, on_delete=models.SET_NULL)
+    last_moderated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="posts_moderated")
     moderated_note = models.TextField()
-
-    # check constraint that a post contains content
+    
     def clean(self):
         super().clean()
+        # check constraint that a post contains content
         if self.content is None:
             raise ValidationError('Add content in order to post.')
-            
-    class Meta:
-        db_constraints = {
-            'post_content_missing': 'CHECK (content IS NOT NULL)',
-        }
-
-    # check constraint that a post moderation note contains content
-    def clean(self):
-        super().clean()
+        # check constraint that a post moderation note contains content
         if self.moderated_note is None:
             raise ValidationError('Notes about moderations need to be included.')
-            
-    class Meta:
-        db_constraints = {
-            'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
-        }
-
-    # check constraint that a post id is set
-    def clean(self):
-        super().clean()
+        # check constraint that a post id is set
         if self.post_id is None:
             raise ValidationError('Post Id must be set.')
-        
+            
     class Meta:
-        db_constraints = {
-            'post_id_missing': 'CHECK (post_id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(content__isnull=False), name="post_content_missing"),
+            #'post_content_missing': 'CHECK (content IS NOT NULL)',
+            models.CheckConstraint(check=models.Q(moderated_note__isnull=False), name="post_moderation_content_missing"),
+            #'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
+            models.CheckConstraint(check=models.Q(post_id__isnull=False), name="post_id_missing")
+            #'post_id_missing': 'CHECK (post_id IS NOT NULL)',
+        ]
+        
 
 class Reply(models.Model):
     # main
@@ -79,47 +68,36 @@ class Reply(models.Model):
     for_post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     for_reply_id = models.ForeignKey('self', on_delete=models.CASCADE)
     datetime_posted = models.DateTimeField()
-    posted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    posted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="replies")
     content = models.TextField()
     datetime_last_edited = models.DateTimeField(null=True)
     
     # moderation
     datetime_last_moderated = models.DateTimeField(null=True)
-    last_moderated_by = models(User, null=True, on_delete=models.SET_NULL)
+    last_moderated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="replies_last_moderated")
     moderated_note = models.TextField()
-
-    # check constraint that a reply contains content
+    
     def clean(self):
         super().clean()
+        # check constraint that a reply contains content
         if self.content is None:
             raise ValidationError('Add content in order to reply.')
-            
-    class Meta:
-        db_constraints = {
-            'reply_content_missing': 'CHECK (content IS NOT NULL)',
-        }
-
-    # check constraint that a reply moderation note contains content
-    def clean(self):
-        super().clean()
+        # check constraint that a reply id is set
+        if self.reply_id is None:
+            raise ValidationError('Reply Id must be set.')
+        # check constraint that a reply moderation note contains content
         if self.moderated_note is None:
             raise ValidationError('Notes about moderations need to be included.')
             
     class Meta:
-        db_constraints = {
-            'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
-        }
-
-    # check constraint that a reply id is set
-    def clean(self):
-        super().clean()
-        if self.reply_id is None:
-            raise ValidationError('Reply Id must be set.')
-        
-    class Meta:
-        db_constraints = {
-            'reply_id_missing': 'CHECK (reply_id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(content__isnull=False), name="reply_content_missing"),
+            #'reply_content_missing': 'CHECK (content IS NOT NULL)',
+            models.CheckConstraint(check=models.Q(moderated_note__isnull=False), name="reply_moderation_content_missing"),
+            #'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
+            models.CheckConstraint(check=models.Q(reply_id__isnull=False), name="reply_id_missing")
+            #'reply_id_missing': 'CHECK (reply_id IS NOT NULL)',
+        ]
 
 # FOR BUILDINGS
 # FOR BUILDINGS
@@ -134,28 +112,24 @@ class Building(models.Model):
     gps_longitude = models.DecimalField(max_digits=9, decimal_places=6)
     gps_latitude = models.DecimalField(max_digits=9, decimal_places=6)
     description = models.TextField()
-
-    # check constraint that a building address is not empty
+    
     def clean(self):
         super().clean()
+        # check constraint that a building address is not empty
         if self.address is None:
             raise ValidationError('Building address must be specified.')
-            
-    class Meta:
-        db_constraints = {
-            'building_address_missing': 'CHECK (address IS NOT NULL)',
-        }
-
-    # check constraint that building description is not empty
-    def clean(self):
-        super().clean()
+        # check constraint that building description is not empty
         if self.description is None:
             raise ValidationError('Building description must be specifed.')
             
     class Meta:
-        db_constraints = {
-            'building_description_missing': 'CHECK (description IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(address__isnull=False), name="building_address_missing"),
+            #'building_address_missing': 'CHECK (address IS NOT NULL)',
+            models.CheckConstraint(check=models.Q(description__isnull=False), name="building_description_missing")
+            #'building_description_missing': 'CHECK (description IS NOT NULL)'
+        ]
+        
     
 class BuildingPost(models.Model):  # many to many linking table for when posts mention buildings
     id = models.AutoField(primary_key=True)
@@ -169,9 +143,10 @@ class BuildingPost(models.Model):  # many to many linking table for when posts m
             raise ValidationError('Id must be set.')
         
     class Meta:
-        db_constraints = {
-            'id_missing': 'CHECK (id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(id__isnull=False), name="buildingpost_id_missing")
+            #'id_missing': 'CHECK (id IS NOT NULL)',
+        ]
     
 class BuildingReply(models.Model): # many to many linking table for when replies mention buildings
     id = models.AutoField(primary_key=True)
@@ -185,9 +160,10 @@ class BuildingReply(models.Model): # many to many linking table for when replies
             raise ValidationError('Id must be set.')
         
     class Meta:
-        db_constraints = {
-            'id_missing': 'CHECK (id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(id__isnull=False), name="buildingreply_id_missing")
+            #'id_missing': 'CHECK (id IS NOT NULL)',
+        ]
 
 class Location(models.Model): # locations within buildings
     location_id = models.AutoField(primary_key=True)
@@ -199,38 +175,30 @@ class Location(models.Model): # locations within buildings
     gps_latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     description = models.TextField()
     
-    # check constraint that either the building or the longitude and latitude must be set
     def clean(self):
         super().clean()
+        # check constraint that either the building or the longitude and latitude must be set
         if self.building_id is None and (self.gps_longitude is None or self.gps_latitude is None):
             raise ValidationError('Either the building or gps coordinates must be specified.')
-        
-    class Meta:
-        db_constraints = {
-            'location_building_or_gps_coords': 'CHECK (building_id IS NOT NULL OR (gps_longitude IS NOT NULL AND gps_latitude IS NOT NULL))',
-        }
-
-    # check constraint that location description is not empty
-    def clean(self):
-        super().clean()
+        # check constraint that location description is not empty
         if self.description is None:
             raise ValidationError('Location description must be specifed.')
-            
-    class Meta:
-        db_constraints = {
-            'location_description_missing': 'CHECK (description IS NOT NULL)',
-        }
-
-    # check constraint that a location id is set
-    def clean(self):
-        super().clean()
+        # check constraint that a location id is set
         if self.location_id is None:
             raise ValidationError('Location Id must be set.')
-            
+        
     class Meta:
-        db_constraints = {
-            'location_id_missing': 'CHECK (location_id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(building_id__isnull=False) | \
+                (models.Q(gps_longitude__isnull=False) & (models.Q(gps_latitude__isnull=False))), \
+                name="location_building_or_gps_coords"),
+            #'location_building_or_gps_coords': 'CHECK (building_id IS NOT NULL OR (gps_longitude IS NOT NULL AND gps_latitude IS NOT NULL))',
+            models.CheckConstraint(check=models.Q(description__isnull=False), name="location_description_missing"),
+            #'location_description_missing': 'CHECK (description IS NOT NULL)',
+            models.CheckConstraint(check=models.Q(location_id__isnull=False), name="location_id_missing")
+            #'location_id_missing': 'CHECK (location_id IS NOT NULL)',
+        ]
+
 
 class LocationPost(models.Model):  # many to many linking table for when posts mention locations
     id = models.AutoField(primary_key=True)
@@ -244,9 +212,10 @@ class LocationPost(models.Model):  # many to many linking table for when posts m
             raise ValidationError('Id must be set.')
         
     class Meta:
-        db_constraints = {
-            'id_missing': 'CHECK (id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(id__isnull=False), name="locationpost_id_missing")
+            #'id_missing': 'CHECK (id IS NOT NULL)',
+        ]
     
 class LocationReply(models.Model): # many to many linking table for when replies mention locations
     id = models.AutoField(primary_key=True)
@@ -260,6 +229,7 @@ class LocationReply(models.Model): # many to many linking table for when replies
             raise ValidationError('Id must be set.')
         
     class Meta:
-        db_constraints = {
-            'id_missing': 'CHECK (id IS NOT NULL)',
-        }
+        constraints = [
+            models.CheckConstraint(check=models.Q(id__isnull=False), name="locationreply_id_missing")
+            #'id_missing': 'CHECK (id IS NOT NULL)',
+        ]

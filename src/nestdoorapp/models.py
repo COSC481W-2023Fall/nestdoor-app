760e8fb5d7acd830c676b7b64666ea7a75c1b7f3
gcolling <gtcolling@hotmail.com>
2023-11-03 11:41:45 -0400

    Quick fix to models -Griff

--- src/nestdoorapp/models.py
+++ src/nestdoorapp/models.py
@@ -28,35 +28,36 @@ class Member(models.Model):
 class Post(models.Model):
     # main
     post_id = models.AutoField(primary_key=True)
-    datetime_posted = models.DateTimeField()
+    title = models.CharField(max_length=255)
+    datetime_posted = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="posts")
     content = models.TextField()
-    number_replies = models.IntegerField()
-    datetime_last_edited = models.DateTimeField(null=True)
+    number_replies = models.IntegerField(default=0)
+    # datetime_last_edited = models.DateTimeField(null=True)
     
     # moderation
-    datetime_last_moderated = models.DateTimeField(null=True)
-    last_moderated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="posts_moderated")
-    moderated_note = models.TextField()
+    # datetime_last_moderated = models.DateTimeField(null=True)
+    # last_moderated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="posts_moderated")
+    # moderated_note = models.TextField()
     
     def clean(self):
         super().clean()
         # check constraint that a post contains content
         if self.content is None:
             raise ValidationError('Add content in order to post.')
-        # check constraint that a post moderation note contains content
-        if self.moderated_note is None:
-            raise ValidationError('Notes about moderations need to be included.')
-        # check constraint that a post id is set
-        if self.post_id is None:
-            raise ValidationError('Post Id must be set.')
+        # # check constraint that a post moderation note contains content
+        # if self.moderated_note is None:
+        #     raise ValidationError('Notes about moderations need to be included.')
+        # # check constraint that a post id is set
+        # if self.post_id is None:
+        #     raise ValidationError('Post Id must be set.')
             
     class Meta:
         constraints = [
             models.CheckConstraint(check=models.Q(content__isnull=False), name="post_content_missing"),
             #'post_content_missing': 'CHECK (content IS NOT NULL)',
-            models.CheckConstraint(check=models.Q(moderated_note__isnull=False), name="post_moderation_content_missing"),
-            #'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
+            # models.CheckConstraint(check=models.Q(moderated_note__isnull=False), name="post_moderation_content_missing"),
+            # #'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
             models.CheckConstraint(check=models.Q(post_id__isnull=False), name="post_id_missing")
             #'post_id_missing': 'CHECK (post_id IS NOT NULL)',
         ]
@@ -66,16 +67,17 @@ class Reply(models.Model):
     # main
     reply_id = models.AutoField(primary_key=True)
     for_post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-    for_reply_id = models.ForeignKey('self', on_delete=models.CASCADE)
-    datetime_posted = models.DateTimeField()
+    #For now all replies are directly to the main post... need to reapproach replying to replies if we implement it.
+    #for_reply_id = models.ForeignKey('self', on_delete=models.CASCADE)
+    datetime_posted = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="replies")
     content = models.TextField()
-    datetime_last_edited = models.DateTimeField(null=True)
+    #datetime_last_edited = models.DateTimeField(null=True)
     
     # moderation
-    datetime_last_moderated = models.DateTimeField(null=True)
-    last_moderated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="replies_last_moderated")
-    moderated_note = models.TextField()
+    # datetime_last_moderated = models.DateTimeField(null=True)
+    # last_moderated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="replies_last_moderated")
+    # moderated_note = models.TextField()
     
     def clean(self):
         super().clean()
@@ -83,17 +85,17 @@ class Reply(models.Model):
         if self.content is None:
             raise ValidationError('Add content in order to reply.')
         # check constraint that a reply id is set
-        if self.reply_id is None:
-            raise ValidationError('Reply Id must be set.')
+        # if self.reply_id is None:
+        #     raise ValidationError('Reply Id must be set.')
         # check constraint that a reply moderation note contains content
-        if self.moderated_note is None:
-            raise ValidationError('Notes about moderations need to be included.')
+        # if self.moderated_note is None:
+        #     raise ValidationError('Notes about moderations need to be included.')
             
     class Meta:
         constraints = [
             models.CheckConstraint(check=models.Q(content__isnull=False), name="reply_content_missing"),
             #'reply_content_missing': 'CHECK (content IS NOT NULL)',
-            models.CheckConstraint(check=models.Q(moderated_note__isnull=False), name="reply_moderation_content_missing"),
+            # models.CheckConstraint(check=models.Q(moderated_note__isnull=False), name="reply_moderation_content_missing"),
             #'post_moderation_content_missing': 'CHECK (moderated_note IS NOT NULL)',
             models.CheckConstraint(check=models.Q(reply_id__isnull=False), name="reply_id_missing")
             #'reply_id_missing': 'CHECK (reply_id IS NOT NULL)',

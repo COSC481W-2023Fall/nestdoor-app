94b54931599004e1f5cdec89e37eb032e5fccf37
JWT2 <144062003+JWT2@users.noreply.github.com>
2023-11-09 07:35:48 -0500

    Fixed bugs.
    Fixed the bugs the last commit introduced.

--- src/nestdoorapp/templatetags/userlink.py
+++ src/nestdoorapp/templatetags/userlink.py
@@ -1,5 +1,4 @@
 from django.template import Library, Node
-from django.db.models import get_model
 from django.template.exceptions import TemplateSyntaxError
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,14 +12,14 @@ class LatestContentNode(Node):
     def render(self, context):
         try:
             user = User.objects.get(id = self.id)
-            return "<a href=\"/User/" + str(self.id) + "\">" + user.username + "</a>"
+            return "<a href=\"/user/" + str(self.id) + "\">" + user.username + "</a>"
         except ObjectDoesNotExist:
             return "Bad user."
  
 def userlink(parser, token):
     bits = token.contents.split()
     if len(bits) != 2:
-        raise TemplateSyntaxError, "user_link tag takes one argument, a user ID"
+        raise TemplateSyntaxError("userlink tag takes one argument, a user ID")
     return LatestContentNode(bits[1])
     
 userlink = register.tag(userlink)
